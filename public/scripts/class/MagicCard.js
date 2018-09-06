3a9873d54552e1f12c2b4d2d3c1c0d792a5b1935
Cyril ANNETTE <zougouda@gmail.com>
2018-09-06 09:56:22 +0200

    fixed get variable when deleting a card

--- public/scripts/class/MagicCard.js
+++ public/scripts/class/MagicCard.js
@@ -480,7 +480,7 @@ class MagicCard
 		{
 			if (newXHR.readyState === 4 && newXHR.status === 200) 
 			{
-				window.location.href = `/list-cards?id=${userID}`;
+				window.location.href = `/list-cards?userID=${userID}`;
 			}
 		};
 		newXHR.open( 'POST', '/delete-card', true );
